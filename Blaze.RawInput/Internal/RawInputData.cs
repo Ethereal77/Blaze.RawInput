ce4fe2b48439b06d0d11c776bb99abc1093f2a98
Mario Guerra <mgg7007@gmail.com>
2020-08-29 22:26:32 +0200

    Unnest RawInputData structures. Added Size field.

--- Blaze.RawInput/Internal/RawInputData.cs
+++ Blaze.RawInput/Internal/RawInputData.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 namespace Blaze.Framework.RawInput
 {
@@ -27,83 +28,93 @@ namespace Blaze.Framework.RawInput
         ///   Data of the RawInput packet.
         /// </summary>
         /// <unmanaged>data</unmanaged>
-        public readonly RawInputInner Data;
+        public readonly RawInputInnerData Data;
 
-        #region Structure: Header
+        #region Static properties
 
         /// <summary>
-        ///   Contains the header information that is part of the raw input data.
+        ///   Size of the <see cref="RawInputData"/> structure, in bytes.
+        /// </summary>
+        public static readonly uint Size = (uint) Unsafe.SizeOf<RawInputData>();
+
+        #endregion
+    }
+
+    /// <summary>
+    ///   Contains the header information that is part of the raw input data.
+    /// </summary>
+    /// <remarks>
+    ///   To get more information on the device, use <see cref="Device"/> in a call to
+    ///   <see cref="RawInput.GetRawInputDeviceInfo"/>.
+    /// </remarks>
+    /// <unmanaged>RAWINPUTHEADER</unmanaged>
+    [StructLayout(LayoutKind.Sequential)]
+    internal readonly struct RawInputHeader
+    {
+        /// <summary>
+        ///   The type of raw input device.
+        /// </summary>
+        /// <unmanaged>dwType</unmanaged>
+        public readonly DeviceType Type;
+
+        /// <summary>
+        ///   The size of the entire input packet of data, in bytes.
         /// </summary>
         /// <remarks>
-        ///   To get more information on the device, use <see cref="Device"/> in a call to
-        ///   <see cref="RawInput.GetRawInputDeviceInfo"/>.
+        ///   This includes <see cref="RawInputData"/> plus possible extra input reports in the <see cref="RawHid"/>
+        ///   variable length array.
         /// </remarks>
-        /// <unmanaged>RAWINPUTHEADER</unmanaged>
-        [StructLayout(LayoutKind.Sequential)]
-        internal readonly struct RawInputHeader
-        {
-            /// <summary>
-            ///   The type of raw input device.
-            /// </summary>
-            /// <unmanaged>dwType</unmanaged>
-            public readonly DeviceType Type;
-
-            /// <summary>
-            ///   The size of the entire input packet of data, in bytes.
-            /// </summary>
-            /// <remarks>
-            ///   This includes <see cref="RawInputData"/> plus possible extra input reports in the <see cref="RawHid"/>
-            ///   variable length array.
-            /// </remarks>
-            /// <unmanaged>dwSize</unmanaged>
-            public readonly int Size;
-
-            /// <summary>
-            ///   The handle of the device generating the raw input data.
-            /// </summary>
-            /// <unmanaged>hDevice</unmanaged>
-            public readonly IntPtr Device;
-
-            /// <summary>
-            ///   The value passed in the  <c>wParam</c> parameter of the <c>WM_INPUT</c> message.
-            /// </summary>
-            /// <unmanaged>wParam</unmanaged>
-            public readonly IntPtr WParam;
-        }
-
-        #endregion
-
-        #region Structure: Data
+        /// <unmanaged>dwSize</unmanaged>
+        public readonly int Size;
 
         /// <summary>
-        ///   Contains the received input that is part of the raw input data.
+        ///   The handle of the device generating the raw input data.
         /// </summary>
-        /// <unmanaged>RAWINPUT_INNER_0</unmanaged>
-        [StructLayout(LayoutKind.Explicit)]
-        internal readonly struct RawInputInner
-        {
-            /// <summary>
-            ///   If the data comes from a mouse, this is the raw input data.
-            /// </summary>
-            /// <unmanaged>mouse</unmanaged>
-            [FieldOffset(0)]
-            public readonly RawMouse Mouse;
+        /// <unmanaged>hDevice</unmanaged>
+        public readonly IntPtr Device;
 
-            /// <summary>
-            ///   If the data comes from a keyboard, this is the raw input data.
-            /// </summary>
-            /// <unmanaged>keyboard</unmanaged>
-            [FieldOffset(0)]
-            public readonly RawKeyboard Keyboard;
+        /// <summary>
+        ///   The value passed in the  <c>wParam</c> parameter of the <c>WM_INPUT</c> message.
+        /// </summary>
+        /// <unmanaged>wParam</unmanaged>
+        public readonly IntPtr WParam;
 
-            /// <summary>
-            ///   If the data comes from an HID, this is the raw input data.
-            /// </summary>
-            /// <unmanaged>hid</unmanaged>
-            [FieldOffset(0)]
-            public readonly RawHid Hid;
-        }
+        #region Static properties
+
+        /// <summary>
+        ///   Size of the <see cref="RawInputHeader"/> structure, in bytes.
+        /// </summary>
+        public static readonly uint HeaderSize = (uint) Unsafe.SizeOf<RawInputHeader>();
 
         #endregion
+    }
+
+    /// <summary>
+    ///   Contains the received input that is part of the raw input data.
+    /// </summary>
+    /// <unmanaged>RAWINPUT_INNER_0</unmanaged>
+    [StructLayout(LayoutKind.Explicit)]
+    internal readonly struct RawInputInnerData
+    {
+        /// <summary>
+        ///   If the data comes from a mouse, this is the raw input data.
+        /// </summary>
+        /// <unmanaged>mouse</unmanaged>
+        [FieldOffset(0)]
+        public readonly RawMouse Mouse;
+
+        /// <summary>
+        ///   If the data comes from a keyboard, this is the raw input data.
+        /// </summary>
+        /// <unmanaged>keyboard</unmanaged>
+        [FieldOffset(0)]
+        public readonly RawKeyboard Keyboard;
+
+        /// <summary>
+        ///   If the data comes from an HID, this is the raw input data.
+        /// </summary>
+        /// <unmanaged>hid</unmanaged>
+        [FieldOffset(0)]
+        public readonly RawHid Hid;
     }
 }
